44ce62da097b0852708941e317b0b0177c795a8b
sauravrawat710 <sauravrawat710@gmail.com>
2024-06-25 13:53:04 +0530

    added routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,6 +7,8 @@ import {
   changeCurrentUserPassword,
   getCurrentUser,
   updateUserAvatar,
+  getUserChannelProfile,
+  getUserWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -33,8 +35,16 @@ router.route("/logout").post(verifyJWT, logoutUser);
 
 router.route("/refresh-token").post(requestAuthToken);
 
-router.route("change-password").post(verifyJWT, changeCurrentUserPassword);
+router.route("/change-password").post(verifyJWT, changeCurrentUserPassword);
 
-router.route("get-current-user").post(verifyJWT, getCurrentUser);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router.route("/channels/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/watch-history").get(verifyJWT, getUserWatchHistory);
 
 export default router;
